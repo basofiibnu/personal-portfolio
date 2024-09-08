1187e37c9ff210d30fe6ec3b66aaebc3e2e7fcc7
basofiibnur <basofiibnur@gmail.com>
2024-09-08 20:29:26 +0700

    feat: setup hero section, added assets, implement hooks for 3d objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Hero from './sections/Hero';
+import Navbar from './sections/Navbar';
+
 const App = () => {
   return (
-    <main className="max-w-7xl mx-auto">
-      <h1 className="text-2xl text-white underline">Hello</h1>
+    <main className="max-w-screen-2xl mx-auto">
+      <Navbar />
+      <Hero />
     </main>
   );
 };
