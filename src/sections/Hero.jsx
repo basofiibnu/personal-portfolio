1a52378989d541c5ef5a671552c4999f51eb7f96
basofiibnur <basofiibnur@gmail.com>
2024-10-24 21:23:44 +0700

    feat: change website

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,21 +28,25 @@ const Hero = () => {
       className="min-h-screen w-full flex flex-col relative"
       id="home"
     >
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 my-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Hi, I am Basofi <span className="waving-hand">👋</span>
+          Hello, I am Basofi <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">
-          Building Products & Brands
+        <p className="hero_tag text-gray_gradient lg:w-[60%] mx-auto">
+          I Code. I Design. I Deliver.
         </p>
       </div>
 
-      <div className="w-full h-full absolute inset-0 mt-10">
+      <div className="w-full h-full absolute inset-0 mt-[80px]">
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             {/* To hide controller */}
             <Leva hidden />
-            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <PerspectiveCamera
+              makeDefault
+              position={[0, 0, 30]}
+              rotation={[0, 0, 0]}
+            />
 
             <HeroCamera isMobile={isMobile}>
               <HackerRoom
