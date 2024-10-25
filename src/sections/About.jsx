0329918b812fad921bf96e6781c1f0bbf0496585
basofiibnur <basofiibnur@gmail.com>
2024-10-25 23:07:19 +0700

    feat: update contents

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
 
   const handleCopy = () => {
     navigator.clipboard.writeText({
-      data: 'https://basofi.vercel.app',
+      data: 'basofiibnur@gmail.com',
     });
     setHasCopied(true);
     setTimeout(() => {
@@ -22,17 +22,16 @@ const About = () => {
           <div className="col-span-1 xl:row-span-3">
             <div className="grid-container">
               <img
-                src="/assets/grid1.png"
+                src="/assets/profile.jpg"
                 alt="grid-1"
-                className="w-full sm:h-[276px] h-fit object-contain"
+                className="w-full h-auto max-h-[276px] object-cover rounded-3xl"
               />
 
               <div>
                 <p className="grid-headtext">Hi, I am Basofi</p>
                 <p className="grid-subtext">
-                  With 12 years of experience, I have honed my skills
-                  in frontend development, with a focus on developing
-                  websites and applications.
+                  An Experienced Developer with 3.5+ Years Focused on
+                  Creating User-Centric, Scalable Front-End Solutions
                 </p>
               </div>
             </div>
@@ -49,40 +48,48 @@ const About = () => {
               <div>
                 <p className="grid-headtext">Tech Stack</p>
                 <p className="grid-subtext">
-                  I specialize in Javascript/Typescript with a focus
-                  on React and Next.js ecosystems.
+                  I specialize in JavaScript/TypeScript with a focus
+                  on React and Next.js ecosystems. Recently, I&apos;ve
+                  added Golang to my skill set as I work towards
+                  becoming a full-stack developer, expanding my
+                  expertise across the entire development stack.
                 </p>
               </div>
             </div>
           </div>
 
           <div className="col-span-1 xl:row-span-4">
-            <div className="grid-container">
+            <div className="grid-container items-center justify-center">
               <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
                 <Globe
-                  width={326}
                   height={326}
-                  backgroundColor="rgba(0,0,0,0)"
+                  width={326}
+                  backgroundColor="rgba(0, 0, 0, 0)"
                   backgroundImageOpacity={0.5}
                   showAtmosphere
                   showGraticules
                   globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
                   bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                  labelsData={{
-                    lat: 40,
-                    lng: -100,
-                    text: "I'm here!",
-                    color: 'white',
-                    size: 20,
-                  }}
+                  labelsData={[
+                    {
+                      lat: 40,
+                      lng: -100,
+                      text: 'Rjieka, Croatia',
+                      color: 'white',
+                      size: 15,
+                    },
+                  ]}
                 />
               </div>
               <div>
                 <p className="grid-headtext">
-                  I work remotely across most timezones
+                  Remote-Ready Developer, Flexible Across Time Zones
                 </p>
                 <p className="grid-subtext">
-                  I am based in Jakarta, with remote work available
+                  Based in Jakarta, Indonesia. I am currently
+                  expanding my skills in Golang to become a full-stack
+                  developer, and I am open to remote work
+                  opportunities.
                 </p>
                 <Button
                   name={'Contact Me'}
@@ -104,16 +111,25 @@ const About = () => {
               <div>
                 <p className="grid-headtext">My Passion for Coding</p>
                 <p className="grid-subtext">
-                  I love solving problems and building things through
-                  code. Coding isn&apos;t just my profession - it is
-                  my passion
+                  I have always had a deep passion for solving
+                  problems and building things through code. To me,
+                  coding is not just a profession, but a creative
+                  outlet where I can bring ideas to life and solve
+                  real-world challenges. There&apos;s something
+                  incredibly fulfilling about breaking down complex
+                  problems, finding solutions, and seeing them take
+                  shape through the power of code. Whether it&apos;s
+                  building seamless user experiences or optimizing the
+                  performance of an application, I am constantly
+                  driven by the potential to make an impact through
+                  technology.
                 </p>
               </div>
             </div>
           </div>
 
           <div className="xl:col-span-1 xl:row-span-2">
-            <div className="grid-container">
+            <div className="grid-container items-center justify-center">
               <img
                 src="/assets/grid4.png"
                 alt="grid-4"
@@ -122,7 +138,7 @@ const About = () => {
 
               <div className="space-y-2">
                 <p className="grid-headtext text-center">
-                  Contact me
+                  Contact me at
                 </p>
                 <div className="copy-container" onClick={handleCopy}>
                   <img
